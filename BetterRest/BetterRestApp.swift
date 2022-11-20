517c300ea05b330d9e42b472f1edb4fd58c039e5
Faizaan Khan <faizaankhan24@gmail.com>
2022-11-19 21:10:34 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Faizaan Khan on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
